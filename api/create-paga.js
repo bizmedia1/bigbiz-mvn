09a81ad443d0313ddd1d3371b3657b45e723f7cc
bizmedia1 <victorisrael539@gmail.com>
2026-07-30 13:06:03 +0100

    Update create-paga.js

--- api/create-paga.js
+++ api/create-paga.js
@@ -15,8 +15,8 @@ export default async function handler(req, res) {
         "Content-Type": "application/json"
       },
       body: JSON.stringify({
-        fname: "John",
-        lname: "Doe"
+        fname: firstName,
+  lname: lastName
       })
     });
 
